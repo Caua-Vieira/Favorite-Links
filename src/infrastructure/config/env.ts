946d2f50f4e7fc93ee36e0850c21613e91125cf4
Caua-Vieira <cauasilva8@gmail.com>
2025-04-03 23:53:46 -0300

    feat: create environment configurations

---
+++ src/infrastructure/config/env.ts
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const env = {
+    dbUser: process.env.DB_USER || "user",
+    dbPass: process.env.DB_PASS || "password",
+    dbName: process.env.DB_NAME || "favorite_links_db",
+    dbHost: process.env.DB_HOST || "localhost",
+    dbPort: Number(process.env.DB_PORT) || 5432,
+    port: Number(process.env.PORT) || 3000,
+};
