16baecc18cbf17743e3eca0c6dba32d2f432d622
Caua-Vieira <cauasilva8@gmail.com>
2025-04-21 20:04:30 -0300

    fix: alterar variáveis para conexão com banco de dados

--- src/infrastructure/config/env.ts
+++ src/infrastructure/config/env.ts
@@ -2,10 +2,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const env = {
-    dbUser: process.env.DB_USER || "user",
-    dbPass: process.env.DB_PASSWORD || "password",
-    dbName: process.env.DB_NAME || "favorite_links_db",
+    dbUser: process.env.DB_USER || "postgres",
+    dbPass: process.env.DB_NAME || "12345",
+    dbName: process.env.DB_PASSWORD || "favorite_links_db",
     dbHost: process.env.DB_HOST || "localhost",
     dbPort: Number(process.env.DB_PORT) || 5432,
     port: Number(process.env.PORT) || 3000,
 };
+
