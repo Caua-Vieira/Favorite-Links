f2e0ca0aac44125b8aab8cfaa73dc6883bcb0a23
Caua-Vieira <cauasilva8@gmail.com>
2025-04-22 23:00:46 -0300

    chore: adicionar ponto e vírgula

--- src/interfaces/middleware/errorHandler.ts
+++ src/interfaces/middleware/errorHandler.ts
@@ -4,12 +4,12 @@ import { DatabaseException, NotFoundException } from "../../domain/errors/errors
 export function errorHandler(err: Error, req: Request, res: Response, next: NextFunction): void {
     if (err instanceof NotFoundException) {
         res.status(404).json({ error: err.message });
-        return
+        return;
     }
 
     if (err instanceof DatabaseException) {
         res.status(500).json({ error: 'Erro no banco de dados: ' + err.message });
-        return
+        return;
     }
 
     res.status(500).json({ error: 'Erro interno do servidor' });
