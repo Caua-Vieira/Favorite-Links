4bead72a3a8187a3325139b8c1556686b34c2c12
Caua-Vieira <cauasilva8@gmail.com>
2025-04-24 00:00:50 -0300

    fix: corrigir nome das variáveis de ambiente

--- src/infrastructure/config/env.ts
+++ src/infrastructure/config/env.ts
@@ -3,8 +3,8 @@ dotenv.config();
 
 export const env = {
     dbUser: process.env.DB_USER || "postgres",
-    dbPass: process.env.DB_NAME || "12345",
-    dbName: process.env.DB_PASSWORD || "favorite_links_db",
+    dbPass: process.env.DB_PASSWORD || "12345",
+    dbName: process.env.DB_NAME || "favorite_links_db",
     dbHost: process.env.DB_HOST || "localhost",
     dbPort: Number(process.env.DB_PORT) || 5432,
     port: Number(process.env.PORT) || 3000,
