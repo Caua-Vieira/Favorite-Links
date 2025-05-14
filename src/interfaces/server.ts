0af81c910aff1025096276d656f04d5600f20a46
Caua-Vieira <cauasilva8@gmail.com>
2025-05-13 23:32:46 -0300

    docs: adicionar documentação Swagger para os endpoints

--- src/interfaces/server.ts
+++ src/interfaces/server.ts
@@ -1,15 +1,19 @@
 import express from "express";
-import { FavoriteLinksController } from "./controller/favoriteLinksController";
+import { FavoriteLinksController } from "./controllers/favoriteLinksController";
 import { Container } from "typescript-ioc";
 import { favoriteLinksRouter } from "./routes/favoriteLinksRoutes";
 import { errorHandler } from "./middleware/errorHandler";
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 const app = express();
 app.use(express.json());
 
 const favoriteLinksController = Container.get(FavoriteLinksController);
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 app.use("/api/favorite-links", favoriteLinksRouter(favoriteLinksController));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(errorHandler);
 
